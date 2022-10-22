66b5a41000864c3081338ecbe8e26768550d2dd2
emiliomelvy <emilio.melvy@mailtarget.co>
2022-10-22 17:43:10 +0700

    working on styling part 1

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,68 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
+  import play from './assets/play.svg'
+  import download from './assets/download.svg'
+  import recycleBin from './assets/recyclebin.svg'
 </script>
 
 <template>
-  <h1 class="text-5xl bg-red-500 text-sky-500">
-    HELLO WORLD
-  </h1>
-</template>
-
-
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="flex justify-around">
+    <div class="pt-10">
+      <div class="w-96 h-96 bg-slate-100 shadow-2xl rounded-full flex justify-center items-center">
+        <div class="w-80 h-80 bg-red-400 rounded-full flex justify-center items-center">
+          <div class="w-64 h-64 bg-red-400 rounded-full flex justify-center border-[2px]">
+            <h1 class="text-5xl text-center text-white my-auto">
+              25:00
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div class="flex justify-between pt-16">
+        <div class="text-5xl w-16 h-16 bg-slate-400 text-center rounded-full cursor-pointer">
+          -
+        </div>
+        <div class="text-5xl w-16 h-16 bg-slate-400 text-center rounded-full cursor-pointer">
+          +
+        </div>
+        <div class="text-5xl w-44 h-16 bg-slate-400 text-center rounded-full flex justify-center items-center cursor-pointer bg-lime-200">
+          <img :src="play" alt="" class="w-8">
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="pt-20">
+      <div class="flex justify-end gap-3">
+        <div class="flex basis-4/12 justify-center gap-2 bg-lime-200 rounded-full cursor-pointer">
+          <img :src="download" alt="" class="w-4 py-2 my-auto">
+          <p class="text-xs py-2 my-auto">
+            Download logs
+          </p>
+        </div>
+        <div class="flex cursor-pointer">
+          <img :src="recycleBin" alt="" class="w-4 py-2 my-auto">
+          <p class="text-xs py-2 my-auto">
+            Delete all sessions
+          </p>
+        </div>
+      </div>
+      <table class="table-auto">
+        <tr class="border rounded-full">
+          <th class="border">#</th>
+          <th class="border">Started at</th>
+          <th class="border">Duration</th>
+          <th class="border">Notes</th>
+        </tr>
+        <tr class="border">
+          <td class="border">1</td>
+          <td class="border">16:27:45, Sep 1, Thu</td>
+          <td class="border"><strong>0:07</strong> / 25 min.</td>
+          <td class="border">  </td>
+        </tr>
+        <tr class="border">
+          <td class="border">2</td>
+          <td class="border">16:28:48, Sep 1, Thu</td>
+          <td class="border"><strong>00:16</strong> / 25 min.</td>
+          <td class="border">makan nasi ayam</td>
+        </tr>
+      </table>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style> -->
