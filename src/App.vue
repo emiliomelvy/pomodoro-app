b1b42c8c02fdbcf9b10d59bc92fa47c61caefc0d
emiliomelvy <emilio.melvy@mailtarget.co>
2022-10-23 14:32:20 +0700

    add notes logic

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,7 @@
         stop,
         timer: null,
         totalTime: (5*60),
+        copyTime: null,
         resetButton: false,
         userTime: 25,
         started: false,
@@ -24,7 +25,8 @@
         date: '',
         days: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
         months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        showModal: true
+        showModal: true,
+        testing: '',
       }
     },
     methods: {
@@ -48,6 +50,8 @@
         clearInterval(this.timer)
         this.timer = null
         this.resetButton = true
+        console.log('copy : ', this.copyTime)
+        console.log(Math.floor(this.copyTime / 60) - this.minutes)
       },
       resetTimer: function() {
         this.arr = [...this.arr, {
@@ -57,8 +61,11 @@
           month: this.date.getMonth(),
           date: this.date.getDate(),
           day: this.date.getDay(),
-          minutes: Math.floor(this.totalTime / 60) < 10 ? `0${Math.floor(this.totalTime / 60)}` : Math.floor(this.totalTime / 60), 
-          sec: this.totalTime % 60 < 10 ? `0${this.totalTime % 60}` : this.totalTime % 60
+          minutes1: Math.floor(this.totalTime / 60) < 10 ? `0${Math.floor(this.totalTime / 60)}` : Math.floor(this.totalTime / 60), 
+          // minutes1: Math.floor((this.totalTime - +this.minutes) / 60),
+          sec: this.totalTime % 60 < 10 ? `0${this.totalTime % 60}` : this.totalTime % 60,
+          // sec: this.totalTime - +this.seconds,
+          note: this.testing
         },
         ]
         clearInterval(this.timer)
@@ -66,7 +73,17 @@
         this.totalTime = (25*60)
         this.resetButton = false
         this.started = false
+        this.testing = ''
+        this.showModal = true
       },
+      enterinput: function(val) {
+        // this.testing += 
+        console.log(val)
+      },
+      get: function(val) {
+        console.log(val
+        )
+      }
     },
     computed: {
       minutes: function(){
@@ -87,12 +104,48 @@
         }
         return this.padTime(seconds);
       }
+    },
+    mounted() {
+      this.copyTime = this.totalTime
     }
   }
 
 </script>
 
 <template>
+  <!-- <div class="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+    <div class="mt-3 text-center">
+      <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
+        <svg class="h-6 w-6 text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+        xmlnx="http://www.w.org/2000/svg">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+          d="M5 13l4 4L19 7">
+          </path>
+        </svg>
+      </div>
+      <h3 class="text-lg leading-6 font-medium text-gray-900">Successfull</h3>
+      <div class="mt-2 px-7 py-3">
+        <p class="text-sm text-gray-500">Account has been Successfull registered.</p>
+      </div>
+      <div class="items-center px-4 py-3">
+        <button id="ok-btn"
+          class="px-4 py-2 bg-red-500 text-white 
+          text-base font-medium rounded-md w-full 
+          shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-purple-300">
+          OK
+        </button>
+      </div>
+    </div>
+  </div> -->
+  <div v-if="showModal" class="absolute shadow-[0 0 0 1000rem rgba(0, 0, 0, 0.5)] w-full h-[100vh]">
+    <div class="relative bg-sky-500 top-40 w-96 mx-auto z-20">
+      <h1 class="text-3xl text-center">
+        Please add notes
+      </h1>
+      <input type="text border-8" placeholder="notes" v-model="this.testing" @keydown.enter="showModal = false">
+      <button @click="showmodal = false" class="bg-red-500">enter</button>
+    </div>
+  </div>
   <div class="container max-w-7xl">
     <div class="flex justify-around">
       <div class="pt-10">
@@ -152,10 +205,10 @@
             <th class="border">Notes</th>
           </tr>
           <tr class="border" v-for="(tes, key) in this.arr" :key="key">
-            <td class="border">1</td>
+            <td class="border">{{ key + 1 }}</td>
             <td class="border">{{ tes.hours }}:{{ tes.mins }}:{{ tes.second }}, {{ this.months[tes.month] }} {{ tes.date }}, {{ this.days[tes.day] }}</td>
-            <td class="border"><strong>{{ tes.minutes }} : {{ tes.sec }}</strong> / 25 min.</td>
-            <td class="border">  </td>
+            <td class="border"><strong>{{ tes.minutes1 }} : {{ tes.sec }}</strong> / 25 min.</td>
+            <td class="border">{{ tes.note }}</td>
           </tr>
         </table>
       </div>
